9d556b563b499d0f0d3ffb4b6cbf6aacb5da246a
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2022-03-16 18:01:25 -0300

    criacao do projeto

---
+++ src/main/java/com/douglas/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
